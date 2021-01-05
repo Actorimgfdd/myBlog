1143b33de627817b8f8bb00b9cd48934693cec11
Your Name <you@example.com>
2021-01-05 20:55:43 +0800

    1月5推送

--- apps/articles/urls.py
+++ apps/articles/urls.py
@@ -4,8 +4,9 @@ from articles import views
 
 
 urlpatterns = [
-    path('list/',views.index,name="list"),
+    path('index/',views.index,name="index"),
     path('detail/<int:pk>',views.detail,name="detail"),
     path('contact/',views.contact,name="contact"),
-    path('about/',views.about,name="about")
+    path('about/',views.about,name="about"),
+    path('search/',views.search,name="search")
 ]
